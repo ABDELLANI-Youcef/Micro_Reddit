b6c104bb750f7699733c09fd3b1220090953644e
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-10-01 00:37:11 +0100

    Setup linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
+# rubocop:disable Layout/LineLength
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :title, presence: true, uniqueness: true
-  validates :body, presence: true, length: { maximum: 1000,  too_long: "%{count} characters is the maximum allowed" }, allow_blank: false, length: { minimum: 5 }
+  validates :body, presence: true, length: { maximum: 1000, too_long: '%<count>s characters is the maximum allowed', minimum: 5 }, allow_blank: false
 end
+# rubocop:enable Layout/LineLength
